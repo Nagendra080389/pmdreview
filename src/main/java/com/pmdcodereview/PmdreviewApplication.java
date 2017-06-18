def82910296b4e3eac62d9aef2bc665ee219e1f5
US\nagesingh <nagesingh@deloitte.com>
2017-06-18 09:30:28 +0530

    Initial commit

---
+++ src/main/java/com/pmdcodereview/PmdreviewApplication.java
@@ -0,0 +1,12 @@
+package com.pmdcodereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdreviewApplication.class, args);
+	}
+}
